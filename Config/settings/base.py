3653c989a166afdd451e0057b6f632f4cb6a53f4
Rayhan <pialshek08@gmail.com>
2024-01-08 18:29:07 +0600

    Bismillah

--- Config/settings/base.py
+++ Config/settings/base.py
@@ -15,12 +15,12 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # Application definition
 INSTALLED_APPS = [
--- Config/settings/development.py
+++ Config/settings/development.py
@@ -4,8 +4,6 @@ from .base import *
 
 DEBUG = True
 ALLOWED_HOSTS = ['*', ]
-# ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
-
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
@@ -29,18 +27,5 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-# Static Root is not for here
-# pial added this for vercel
-
-
-# print(STATICFILES_DIRS)
-# print("000000000000000000000000000000000000000000")
-# static/files
-# STATIC_ROOT = '/path/to/static/files'
-# print(STATIC_ROOT)
-# print("000000000000000000000000000000000000000000")
-# /path/to/static/files
-# STATIC_ROOT = config('STATIC_ROOT')
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
