0d720556aa3277537aa674c1bfebf0909cfcd742
Rayhan <pialshek08@gmail.com>
2024-01-08 17:46:01 +0600

    changed sa

--- Config/wsgi.py
+++ Config/wsgi.py
@@ -11,8 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Config.settings.production')
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Config.settings.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Config.settings.production')
 
 application = get_wsgi_application()
 app = application
