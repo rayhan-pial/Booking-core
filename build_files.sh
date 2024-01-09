5da96946e513b1cfda4cdef43fc7594290257605
Rayhan <pialshek08@gmail.com>
2024-01-09 14:29:31 +0600

    added migrate and migrations

--- build_files.sh
+++ build_files.sh
@@ -6,3 +6,10 @@
 # build_files.sh
 pip install -r requirements.txt
 python3.9 manage.py collectstatic
+
+echo "Make Migrations"
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+echo "Collect Static"
+python3.9 manage.py collectstatic --noinput -- clear
